09ba61b380a27ef901aff6596cd520e5099934ab
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 02:07:26 -0700

    update app.js

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.static('build'));
 
+// PUT ROUTES BELOW HERE
+
+//
+
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`))
+
 module.exports = app;

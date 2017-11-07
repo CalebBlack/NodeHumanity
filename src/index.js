72a089b072781e700af36bd4eb14fe98b0308270
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 01:56:32 -0700

    wait for page load

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const ReactDOM = require('react-dom');
 const React = require('react');
 const App = require('./app');
 
-ReactDOM.render(App,document.body);
+document.addEventListener('load',()=>{
+  ReactDOM.render(App,document.body);
+})

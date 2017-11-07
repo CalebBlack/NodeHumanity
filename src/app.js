8475279b122111847736bc0936be39799b31405c
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 00:36:58 -0700

    create src folder

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const React = require('react');
+class App extends React.Component {
+  render(){
+    return (<h1>hi</h1>);
+  }
+}
+module.exports = App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const ReactDOM = require('react-dom');
+const React = require('react');
+const App = require('./app');
+
+ReactDom.renderTo(App,document.body);

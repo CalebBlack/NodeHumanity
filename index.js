802afd1bf3395b040968d1386e8c8ea7626688b8
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 15:19:05 -0700

    update index.js to check if source is built

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const app = require('./app');
 const stringToInt = require('./functions/stringtoint');
+const fs = require('fs');
 
 const port = process.argv[2] ? stringToInt(process.argv[2]) : 8080;
 
 if (port === null) {
-  throw new Error('Cannot Start, Invalid Port Argument.');
+  throw new Error('Invalid Port Argument.');
 }
+
+
+if (!fs.existsSync('./build/source.js')) throw new Error('Source Not Built! Type "npm run build"');
+
 
 try {
   const cards = require('./public/cards.json');
 } catch (error) {
-  throw new Error('Cannot Start, Missing Card List');
+  throw new Error('Cannot Start, Missing/Invalid Card List');
 }
 
 app.listen(port,err=>{

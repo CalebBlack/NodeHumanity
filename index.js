6effa661f7860a25022837f6eaf06569fb044c94
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-06 22:58:27 -0700

    update index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const stringToInt = require('./functions/stringtoint');
 
-const port = process.argv[2] ? stringToInt(proces.argv[2]) : null;
+const port = process.argv[2] ? stringToInt(process.argv[2]) : null;
 
 if (port === null) {
   throw new Error('Cannot Start, Invalid or Missing Port Argument.');

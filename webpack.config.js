579e9a8cd2385a4d76d43c04ed9d9cb44a9863ed
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 01:53:06 -0700

    update webpack config and dependencies for react jsx syntax

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     filename: "source.js",
     path: __dirname +"/build"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react']
+        }
+      }
+    ]
+  },
   entry: "./src/index.js"
 }

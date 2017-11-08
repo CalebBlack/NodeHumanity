e24e4351999038a892e85177ebd5aa04b723a1fa
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 20:00:46 -0700

    add login status to reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,14 @@
 import * as actionTypes from './actiontypes';
-const initialState = {}
+import * as loginStatuses from './loginstatuses';
+const initialState = {loginStatus:loginStatuses.uninitialized};
 
 function reducer(state, action) {
   if (typeof state === 'undefined') {
     return initialState;
   }
   switch(action.type) {
+    case setLoginStatus:
+      return Object.assign({},state,{loginStatus:action.loginStatus});
     case actionTypes.setCardDatabase:
       return Object.assign({},state,{cards:action.cards,blackCards:action.cards.blackCards,whiteCards:action.cards.whiteCards});
     default:

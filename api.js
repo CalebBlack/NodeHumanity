b7889e5371f27e80347c936b025cf0b5d69aa06d
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 22:29:04 -0700

    update api

--- api.js
+++ api.js
@@ -3,12 +3,15 @@ const api = express.Router()
 
 
 // define the home page route
-api.get('/', function (req, res) {
+api.get('/',(req, res)=>{
   res.status(200).send('Cards Against Humanity API');
 });
 // define the about route
-api.get('/about', function (req, res) {
+api.get('/about', (req, res)=>{
   res.send('About birds')
-})
+});
+api.get('/login',(req,res)=>{
+  res.status(400).send('error');
+});
 
 module.exports = api

ca3817d30efe81f9cc44374ca447425f369ebe66
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-08 00:57:53 -0700

    update reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,6 @@
 import * as actionTypes from './actiontypes';
 import * as loginStatuses from './loginstatuses';
-const initialState = {loginStatus:loginStatuses.uninitialized};
+const initialState = {loggedIn:false,loginStatus:loginStatuses.uninitialized};
 
 function reducer(state, action) {
   if (typeof state === 'undefined') {
@@ -8,7 +8,7 @@ function reducer(state, action) {
   }
   switch(action.type) {
     case actionTypes.setLoginStatus:
-      return Object.assign({},state,{loginStatus:action.loginStatus});
+      return Object.assign({},state,{loggedIn:action.loginStatus === loginStatuses.loggedIn, loginStatus:action.loginStatus});
     case actionTypes.setCardDatabase:
       return Object.assign({},state,{cards:action.cards,blackCards:action.cards.blackCards,whiteCards:action.cards.whiteCards});
     default:

d85d11c1f79670391976c6ba0635025ae60f5c6b
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-07 22:08:52 -0700

    update login

--- src/redux/actions/login.js
+++ src/redux/actions/login.js
@@ -1,8 +1,16 @@
 import * as loginStatuses from '../loginstatuses';
 import {setLoginStatus} from '../actiontypes';
+import request from '../../functions/request';
 
 export function login(username,password) {
   return dispatch=>{
     dispatch({type:setLoginStatus,loginStatus:loginStatuses.loggingIn});
+    request('/api/login',null,{auth:[username,password]}).then(response=>{
+      console.log(response);
+      dispatch({type:setLoginStatus,loginStatus:loginStatuses.loggedIn});
+    }).catch(err=>{
+      console.log(error);
+      dispatch({type:setLoginStatus,loginStatus:loginStatuses.failed});
+    });
   }
 }

054c51b072e56bc182778c6862a8f0be5e580508
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-09 22:22:59 -0700

    install helmet

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 const api = require('./api');
 
+app.disable('x-powered-by');
+
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 app.use(express.static('public'));
 app.use(express.static('build'));

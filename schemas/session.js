fce63943ee9a9440c7dcd788b09c596c34d04517
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-10 01:21:14 -0700

    make token id's secure

--- schemas/session.js
+++ schemas/session.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto');
 module.exports = {
   owner: {type:String,unique:true,required:true},
   created: {type:Date,default: Date.now,required:true},
-  createdAt: { type: Date, expires: 30*24*60*60, default: Date.now }
+  createdAt: { type: Date, expires: 30*24*60*60, default: Date.now },
+  _id: {type:Number,required:true,default:()=>{return crypto.randomBytes(64).toString('hex')}}
 }

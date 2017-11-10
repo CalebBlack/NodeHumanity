608c702b000af71506e8eba8662e4ccd614982d7
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-10 05:36:19 -0700

    add auth to socket.io

--- sockets.js
+++ sockets.js
@@ -1,4 +1,31 @@
-function sockets(app){
+const {Session} = require('./models');
 
+function sockets(app){
+  let server = require('http').createServer(app);
+  let io = require('socket.io')(server);
+
+  io.use(function(socket, next) {
+  var token = socket.request.query.token;
+  if (token) {
+    Session.findOne({_id:token},(err,token)=>{
+      if (err || !token) return next(new Error('Unauthorized'));
+      next();
+    })
+  } else {
+    next(new Error('Unauthorized'));
+  }
+  // checkAuthToken(token, function(err, authorized){
+  //   if (err || !authorized) {
+  //     next(new Error("not authorized"));
+  //   }
+  //   next();
+  //   });
+  });
+
+  io.on('connection', function (socket) {
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 }
 module.exports = sockets;

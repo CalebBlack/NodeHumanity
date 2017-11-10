1909e10260092e9890866a2e2aaae0b553411e19
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-10 04:57:58 -0700

    call auth initialize in app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Router from './router';
 import Background from './components/background';
 import getCardDatabase from './redux/actions/getCardDatabase';
+import {initialize} from './redux/actions/login';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import Header from './components/header';
@@ -10,6 +11,7 @@ import './app.css';
 class App extends React.Component {
   componentDidMount(){
     this.props.dispatch(getCardDatabase());
+    this.props.dispatch(initialize());
   }
   render(){
     return (

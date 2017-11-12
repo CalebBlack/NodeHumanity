2b46a6e07303a656e793858196234c2835bdbb46
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-12 16:18:53 -0700

    block certain words

--- filterchat.js
+++ filterchat.js
@@ -1,6 +1,9 @@
 const anchorme = require('anchorme').default;
+const bannedWords = require('./bannedwords.json');
 module.exports = function filterChat(message){
-  if (message.includes('href=')) return false;
+  for (var i = 0; i < bannedWords.length; i++) {
+    if (message.includes(bannedWords[i])) return true;
+  }
   let detect = anchorme(message,{list:true});
   if (detect.length > 0) return true;
   return false;

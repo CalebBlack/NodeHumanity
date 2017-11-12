877676e4949601d870ead1738eb76c44e1a137de
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-12 16:24:24 -0700

    improve filter accuracy

--- filterchat.js
+++ filterchat.js
@@ -1,8 +1,10 @@
 const anchorme = require('anchorme').default;
 const bannedWords = require('./bannedwords.json');
 module.exports = function filterChat(message){
+  var lowercase = message.toLowerCase();
+  var nospace = lowercase.replace(' ','');
   for (var i = 0; i < bannedWords.length; i++) {
-    if (message.includes(bannedWords[i])) return true;
+    if (nospace.includes(bannedWords[i])) return true;
   }
   let detect = anchorme(message,{list:true});
   if (detect.length > 0) return true;

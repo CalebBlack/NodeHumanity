a7658057f52adcab8af9fae814d7541e143c5ee0
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-11-13 09:06:39 -0700

    migrate socket listeners to class methods

--- gamerunner.js
+++ gamerunner.js
@@ -2,13 +2,14 @@ const cards = require('./public/cards.json');
 const randomBetween = require('./functions/randombetween');
 const minimumPlayers = 1;
 
-function drawCard(black=false){
+function drawCard(black = false) {
   let color = black !== true ? 'whiteCards' : 'blackCards';
-  return randomBetween(0,cards[color].length -1);
+  return randomBetween(0, cards[color].length - 1);
 }
-function drawHand(){
+
+function drawHand() {
   var output = [];
-  for (var i = 0; i < 10; i++){
+  for (var i = 0; i < 10; i++) {
     output.push(drawCard(false));
   }
   return output;
@@ -23,7 +24,9 @@ class GameRunner {
     this.drawAll = this.drawAll.bind(this);
     this.round = this.round.bind(this);
     this.winner = this.winner.bind(this);
+    this.chooseCard = this.chooseCard.bind(this);
     this.roundTimeout = this.roundTimeout.bind(this);
+    this.chooseWinner = this.chooseWinner.bind(this);
     this.roundTimeLimit = 10000;
     this.checkStart = this.checkStart.bind(this);
     this.started = false;
@@ -37,77 +40,87 @@ class GameRunner {
     this.selections = {};
     this.roundNumber = 1;
   }
-  round(){
+  round() {
     this.stage = 1;
     this.roundNumber++;
     this.selections = {};
     this.blackCard = drawCard(true);
-    this.emit('newround',{round:this.roundNumber,blackCard:this.blackCard});
+    this.emit('newround', {
+      round: this.roundNumber,
+      blackCard: this.blackCard
+    });
     this.drawAll();
   }
-  drawAll(){
-    Object.entries(this.hands).forEach(handPair=>{
+  drawAll() {
+    Object.entries(this.hands).forEach(handPair => {
       let hand = handPair[1];
       let card = drawCard(false);
       hand.push(card);
-      this.room.players[handPair[0]].emit('drewCard',card);
+      this.room.players[handPair[0]].emit('drewCard', card);
     });
   }
-  nextRound(){
+  nextRound() {
     clearInterval(this.interval);
-    this.interval = setInterval(this.roundTimeout,this.roundTimeLimit);
+    this.interval = setInterval(this.roundTimeout, this.roundTimeLimit);
     this.round();
   }
-  disconnected(socket){
+  disconnected(socket) {
 
   }
-  stage2(){
+  stage2() {
     this.stage = 2;
-    this.emit('roundstage',this.stage);
+    this.emit('roundstage', this.stage);
+  }
+  chooseCard(id) {
+    if (typeof id != 'number' || this.stage != 1 || socket === this.cardCzar) return;
+    let hand = this.hands[socket.id];
+    if (hand.includes(id)) {
+      this.selections[socket.id] = id;
+    }
   }
   connected(socket) {
     this.checkStart();
-    socket.on('choosecard',id=>{
-      if (typeof id != 'number' || this.stage != 1 || socket === this.cardCzar) return;
-      let hand = this.hands[socket.id];
-      if (hand.includes(id)) {
-        this.selections[socket.id] = id;
-      }
-    });
-    socket.on('choosewinner',id=>{
-      if (typeof id == 'number' && this.stage == 2 && this.room.players[id] && this.cardCzar === socket.id) {
-        if (!this.winner(id)) {
-          this.nextRound();
-        }
-      }
-    })
+    socket.on('choosecard', this.chooseCard);
+    socket.on('choosewinner', this.chooseWinner);
   }
-  winner(id){
-    this.emit('roundwinner',{user:this.room.sockets[id].user.username});
+  chooseWinner(id) {
+    if (typeof id == 'number' && this.stage == 2 && this.room.players[id] && this.cardCzar === socket.id) {
+      if (!this.winner(id)) {
+        this.nextRound();
+      }
+    }
+  }
+  winner(id) {
+    this.emit('roundwinner', {
+      user: this.room.sockets[id].user.username
+    });
     this.wins[id] = (this.wins[id] || 0) + 1;
     return this.wins[id] >= this.winAmount;
   }
-  checkStart(){
+  checkStart() {
     if (!this.started) {
       if (Object.keys(this.room.players).length >= minimumPlayers) {
         this.start();
       }
     }
   }
-  roundTimeout(){
+  roundTimeout() {
     this.emit('roundtimeout');
     this.round();
   }
-  start(){
+  start() {
     this.started = true;
-    this.emit('gamestarting',{round:this.roundNumber,blackCard:drawCard(true)});
-    this.interval = setInterval(this.roundTimeout,this.roundTimeLimit);
-    Object.values(this.room.players).forEach(socket=>{
+    this.emit('gamestarting', {
+      round: this.roundNumber,
+      blackCard: drawCard(true)
+    });
+    this.interval = setInterval(this.roundTimeout, this.roundTimeLimit);
+    Object.values(this.room.players).forEach(socket => {
       this.hands[socket.id] = drawHand();
-      socket.emit('startinghand',this.hands[socket.id]);
+      socket.emit('startinghand', this.hands[socket.id]);
     });
   }
-  destroy(){
+  destroy() {
     if (this.interval) this.interval = clearInterval(this.interval);
   }
 }

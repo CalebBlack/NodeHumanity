f1d1bc13e321e1c7bc50de5385d5323f6e34d22a
Caleb Black <caleb.paul.black@gmail.com>
2017-11-16 16:45:53 -0700

    disable enforce https

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const http = require('http');
 const compression = require('compression');
-const enforce = require('express-sslify');
+//const enforce = require('express-sslify');
 
 const api = require('./api');
 const {sockets} = require('./sockets');
@@ -15,7 +15,7 @@ const server = http.Server(app);
 app.disable('x-powered-by');
 
 // MIDDLEWARE
-app.use(enforce.HTTPS());
+//app.use(enforce.HTTPS());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(helmet());

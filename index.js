7b9b66af0ef64e0240645ad8464b445f53b77be2
Caleb Black <caleb.paul.black@gmail.com>
2017-11-16 23:53:14 -0700

    update index to support https and http

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = require('./app');
 const stringToInt = require('./functions/stringtoint');
 const fs = require('fs');
 
-const port = process.argv[2] ? stringToInt(process.argv[2]) : 8080;
+const port = process.argv[2] ? stringToInt(process.argv[2]) : null;
 
 if (port === null) {
   throw new Error('Invalid Port Argument.');
@@ -18,7 +18,25 @@ if (port === null) {
 if (!fs.existsSync('./build/source.js')) throw new Error('Source Not Built! Type "npm run build"');
 
 
-app.listen(port,err=>{
-  if (err) throw err;
-  console.log(`Server Running on Port ${port}.`);
-});
+if (port) {
+  app.listen(port,err=>{
+    if (err) throw err;
+    console.log(`Server Running on Port ${port}.`);
+  });
+} else {
+  if (fs.existsSync('sslcert/server.key') && fs.existsSync('sslcert/server.crt')) {
+    var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+    var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+    var httpServer = http.createServer(app);
+    var httpsServer = https.createServer(credentials, app);
+    console.log('Launching Secure and Insecure HTTP servers');
+    httpServer.listen(80);
+    httpsServer.listen(443);
+  } else {
+    app.listen(80,err=>{
+      if (err) throw err;
+      console.log(`Server Running on Port 80.`);
+    });
+  }
+}

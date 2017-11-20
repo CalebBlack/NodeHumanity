ba66164b7bd31c6cee264e9c6ec471de2c80bf3b
Caleb Black <caleb.paul.black@gmail.com>
2017-11-19 23:15:50 -0700

    remove unique from session schema due to mongoose error

--- schemas/session.js
+++ schemas/session.js
@@ -3,5 +3,5 @@ module.exports = {
   owner: {type:String,unique:true,required:true},
   created: {type:Date,default: Date.now,required:true},
   createdAt: { type: Date, expires: 30*24*60*60, default: Date.now },
-  _id: {type:String,unique:true,required:true,default:()=>{return crypto.randomBytes(48).toString('base64')}}
+  _id: {type:String,required:true,default:()=>{return crypto.randomBytes(48).toString('base64')}}
 }
